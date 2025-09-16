b39f7d6508be158032ca2570c000be812c431827
andrecesarhdev <andrecesarhenrique@gmail.com>
2025-09-16 15:09:57 -0300

    adiciona entidade Categoria ao modelo

---
+++ src/main/java/com/farmacia/demofarmacia/model/Categoria.java
@@ -0,0 +1,43 @@
+package com.farmacia.demofarmacia.model;
+
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+//@Table(name = "categorias")
+public class Categoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String descricao;
+
+    private @OneToMany(mappedBy = "categoria") List<Produto> produtos;
+
+    // Getters e Setters
+    public Long getId() {
+        return id;
+    } 
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }   
+    public List<Produto> getProdutos() {
+        return produtos;
+    }
+    public void setProdutos(List<Produto> produtos) {
+        this.produtos = produtos;
+    }
+}
